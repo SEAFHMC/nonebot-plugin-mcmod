b9320090c2f4e214d79b8ea3872673b863f83643
iqach <ljq9102@outlook.com>
2025-04-11 23:54:13 +0800

    完善项目结构

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 通过https://www.mcmod.cn获取模组、整合包、教程等信息
 
 ## TODO
+![alt text](img/image.png)
+![alt text](img/image-1.png)
--- nonebot-plugin-mcmod/__init__.py
+++ nonebot-plugin-mcmod/__init__.py
@@ -4,10 +4,10 @@ from .mcmod import wiki
 
 __plugin_meta__ = PluginMetadata(
     name="mcmod百科插件",
-    description="通过mcmod.cn获取模组、整合包、教程等信息",
+    description="通过 https://www.mcmod.cn 获取模组、整合包、教程等信息",
     usage="#百科模组 <模组名> | #百科整合包 <整合包名> | #百科教程 <教程名> | #百科资料 <资料名> | #百科 <模组名/整合包名/教程名/资料名> [序号]",
     type="application",
-    homepage="https://github.com/chrysophon/nonebot-plugin-mcmod",
+    homepage="https://github.com/chrysoljq/nonebot-plugin-mcmod",
 
     supported_adapters={"~onebot.v11"},
 )
